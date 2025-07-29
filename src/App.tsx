0e351b019ca79b849495c11a885a63dd628d166f
JuiceBox7 <juiceinbox7@gmail.com>
2025-07-29 03:25:01 -0700

    setting up dependencies

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <p>
+          Currently being redone to better showcase talents, skills, experience,
+          and projects
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
